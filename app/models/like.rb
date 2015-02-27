a9d66b6ccc53808a2ca0fea986ca81f84ed41fb2
heyjoeb <heyjoeb@hotmail.com>
2015-02-27 16:43:51 -0500

    implementación correos de notificacione para registro y matches

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  friend_id  :integer
+#  liked      :boolean
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Like < ActiveRecord::Base
   belongs_to :user
 end
